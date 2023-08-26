8f8f9023ec3716e4ef555678312afd4aa40efea2
Iamneo <techadmin@iamneo.ai>
2023-08-26 08:39:18 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,26 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { InterfaceComponent } from './interface/interface.component';
-import { NewFoodComponent } from './new-food/new-food.component';
 import { TodayComponent } from './today/today.component';
+import { NewFoodComponent } from './new-food/new-food.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    InterfaceComponent,
+    TodayComponent,
     NewFoodComponent,
-    TodayComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    // HeaderComponent,
+    // NewFoodComponent,
   ],
-  providers: [],
+  providers: [],schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
