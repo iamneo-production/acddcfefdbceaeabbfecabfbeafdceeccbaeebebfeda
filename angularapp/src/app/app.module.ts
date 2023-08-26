e47ba1c1c24e83b2620687296f17ac8fd1382a6f
Iamneo <techadmin@iamneo.ai>
2023-08-26 08:35:20 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { InterfaceComponent } from './interface/interface.component';
+import { NewFoodComponent } from './new-food/new-food.component';
+import { TodayComponent } from './today/today.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    InterfaceComponent,
+    NewFoodComponent,
+    TodayComponent
   ],
   imports: [
     BrowserModule,
